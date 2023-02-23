9be5d7771588633cca1da68306081553b2f5a38e
Giang Huynh <trgianghuynhdev1808@gmail.com>
2023-02-23 10:17:31 +0700

    write docker-compose file

--- kafka-server.sh
+++ kafka-server.sh
@@ -27,3 +27,71 @@ echo "MaxSessions 50" >>/etc/ssh/sshd_config
 
 service ssh restart
 
+touch docker-compose.yml
+
+echo '
+version: "3"
+services:
+  zookeeper:
+    image: confluentinc/cp-zookeeper:7.3.0
+    container_name: zookeeper
+    environment:
+      ZOOKEEPER_CLIENT_PORT: 2181
+      ZOOKEEPER_TICK_TIME: 2000
+    networks:
+      - kafka-network
+
+  broker:
+    image: confluentinc/cp-kafka:7.3.0
+    container_name: broker
+    ports:
+      - "9092:9092"
+    depends_on:
+      - zookeeper
+    environment:
+      KAFKA_BROKER_ID: 1
+      KAFKA_ZOOKEEPER_CONNECT: "zookeeper:2181"
+      KAFKA_LISTENER_SECURITY_PROTOCOL_MAP: PLAINTEXT:PLAINTEXT,PLAINTEXT_INTERNAL:PLAINTEXT
+      KAFKA_ADVERTISED_LISTENERS: PLAINTEXT://broker:9092,PLAINTEXT_INTERNAL://broker:29092
+      KAFKA_OFFSETS_TOPIC_REPLICATION_FACTOR: 1
+      KAFKA_TRANSACTION_STATE_LOG_MIN_ISR: 1
+      KAFKA_TRANSACTION_STATE_LOG_REPLICATION_FACTOR: 1
+    networks:
+      - kafka-network
+
+  kafka-ui:
+    image: provectuslabs/kafka-ui
+    container_name: kafka-ui
+    ports:
+      - "8080:8080"
+    restart: always
+    environment:
+      KAFKA_CLUSTERS_0_NAME: local
+      KAFKA_CLUSTERS_0_BOOTSTRAPSERVERS: broker:9092
+      KAFKA_CLUSTERS_0_ZOOKEEPER: zookeeper:2181
+    depends_on:
+      - broker
+      - zookeeper
+    networks:
+      - kafka-network
+
+  kafka-producer:
+    image: kafka/producer
+    container_name: kafka-producer
+    ports:
+      - "3000:3000"
+    networks:
+      - kafka-network
+
+  kafka-consumer:
+    image: kafka/consumer
+    container_name: kafka-consumer
+    ports:
+      - "3001:3001"
+    networks:
+      - kafka-network
+
+networks:
+  kafka-network:
+    driver: bridge
+' >> ~/docker-compose.yml
