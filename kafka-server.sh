39acf65cf075fc9146b976bfc5b90528a838564c
Giang Huynh <trgianghuynhdev1808@gmail.com>
2023-02-23 09:19:56 +0700

    update bash script

--- kafka-server.sh
+++ kafka-server.sh
@@ -1,4 +1,19 @@
- # INSTALL DOCKER
+#!/bin/bash
+# CREATE SWAP
+sudo fallocate -l 4G /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+sudo cat /etc/fstab swap swap defaults 0 0 >>/swapfile
+
+# INSTALL SSH CONNECT
+mkdir -p ~/.ssh
+echo '${var.ssh_public_key}' >>~/.ssh/authorized_keys
+chmod 700 ~/.ssh
+chmod 600 ~/.ssh/authorized_keys
+service ssh restart
+
+# INSTALL DOCKER
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 
