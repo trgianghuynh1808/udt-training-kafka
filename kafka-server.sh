9159f6f7e2f3b87f5f0890da92fd774f59ff2e6e
Giang Huynh <trgianghuynhdev1808@gmail.com>
2023-02-28 09:56:18 +0700

    push docker-compose file

--- kafka-server.sh
+++ kafka-server.sh
@@ -8,62 +8,6 @@ sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
-
-cat << EOF > docker-compose.yml
-hey hey
-yo
-EOF
-
-# CREATE DOKCER-COMPOSE file
-# echo '
-# version: "3"
-# services:
-#   zookeeper:
-#     image: confluentinc/cp-zookeeper:7.3.0
-#     container_name: zookeeper
-#     environment:
-#       ZOOKEEPER_CLIENT_PORT: 2181
-#       ZOOKEEPER_TICK_TIME: 2000
-#     networks:
-#       - kafka-network
-
-#   broker:
-#     image: confluentinc/cp-kafka:7.3.0
-#     container_name: broker
-#     ports:
-#       - "9092:9092"
-#     depends_on:
-#       - zookeeper
-#     environment:
-#       KAFKA_BROKER_ID: 1
-#       KAFKA_ZOOKEEPER_CONNECT: "zookeeper:2181"
-#       KAFKA_LISTENER_SECURITY_PROTOCOL_MAP: PLAINTEXT:PLAINTEXT,PLAINTEXT_INTERNAL:PLAINTEXT
-#       KAFKA_ADVERTISED_LISTENERS: PLAINTEXT://broker:9092,PLAINTEXT_INTERNAL://broker:29092
-#       KAFKA_OFFSETS_TOPIC_REPLICATION_FACTOR: 1
-#       KAFKA_TRANSACTION_STATE_LOG_MIN_ISR: 1
-#       KAFKA_TRANSACTION_STATE_LOG_REPLICATION_FACTOR: 1
-#     networks:
-#       - kafka-network
-
-#   kafka-ui:
-#     image: provectuslabs/kafka-ui
-#     container_name: kafka-ui
-#     ports:
-#       - "8080:8080"
-#     restart: always
-#     environment:
-#       KAFKA_CLUSTERS_0_NAME: local
-#       KAFKA_CLUSTERS_0_BOOTSTRAPSERVERS: broker:9092
-#       KAFKA_CLUSTERS_0_ZOOKEEPER: zookeeper:2181
-#     depends_on:
-#       - broker
-#       - zookeeper
-#     networks:
-#       - kafka-network
-
-# networks:
-#   kafka-network:
-#     driver: bridge
-# ' >> docker-compose.yml
-
-# docker-compose up
+sudo apt install git
+git clone https://github.com/trgianghuynh1808/udt-training-kafka.git
+cd udt-training-kafka
